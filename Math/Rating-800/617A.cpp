4b5f8dcb0175fa8994171a7e2fac748e16df663d
lhminh17 <luuhoangminh2k6@icloud.com>
2025-11-29 23:47:06 +0700

    18

---
+++ Math/Rating-800/617A.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x; cin >>x;
+    int count = 0;
+    if(x <= 5) cout << 1 << endl;
+    else{
+        while(x > 5){
+            count++;
+            x -= 5;
+        }
+        cout << x + 1 <<endl;
+    }
+    return 0;
+}
