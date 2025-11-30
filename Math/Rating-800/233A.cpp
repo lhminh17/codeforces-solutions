447c67ba2214cc310f58f196da511e428519f38e
mèo béo <luuhoangminh2k6@icloud.com>
2025-12-01 01:50:27 +0700

    27
    1/12/2025

---
+++ Math/Rating-800/233A.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    int n; cin >>n;
+    
+	if (n %2 != 0) cout << -1 <<endl;
+	else{
+		for(int i=1 ;i< n + 1; i++){
+			if(i % 2 != 0) cout << i + 1 << " ";
+			else cout << i - 1 << " "; 
+		}
+	}
+    return 0;
+}
