0bce3215c13585676d2c5d6a7c68f4dece30a82f
mèo béo <luuhoangminh2k6@icloud.com>
2025-11-30 14:45:28 +0700

    23

---
+++ Math/Rating-800/2086A.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int t; cin >>t;
+    while(t--){
+        long long n; cin >>n;
+        long long x = 3*n;
+        cout << (2*x)/3 <<endl;
+    }
+    return 0;
+}
