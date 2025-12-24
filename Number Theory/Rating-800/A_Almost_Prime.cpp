06b78fc78b3d47f3a93f21296e688ce6e4cb30bd
lhminh17 <luuhoangminh2k6@icloud.com>
2025-12-25 02:31:53 +0700

    63

---
+++ Number Theory/Rating-800/A_Almost_Prime.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#define f0(i,n) for(int i=0; i<n; i++)
+#define f1(i,n) for(int i=0; i<n-1; i++)
+#define f(i,n) for(int i=0; i<n; i++) cin >> v[i] 
+#define ff(i,m) for(int i=0; i<m; i++) cin >> u[i]
+#define fi first
+#define se second
+#define ll long long 
+const int  maxn  = 200005;
+
+using namespace std;
+
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	
+    int n; cin >> n;
+    int v[3001] = {0};
+
+    for(int i = 2; i<= n; i++){
+        if(v[i] == 0){
+            for(int j = i; j<= n; j+= i){
+                v[j]++;
+            }
+        }
+    }
+
+    int r= 0 ;
+    for(int i=2; i<=n; i++){
+        if(v[i] == 2) r++;
+    }
+
+    cout << r << endl;
+	
+    return 0;	   
+}
